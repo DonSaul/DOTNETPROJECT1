42b6762dbc5225a5dbdd4d1feda5e78f5b913eae
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2024-01-15 17:54:14 -0300

    Creado método base para devolver ReportData

--- DEMO2/Softserve.ProjectLab.ClientAPI/Services/WorkOrderService.cs
+++ DEMO2/Softserve.ProjectLab.ClientAPI/Services/WorkOrderService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Softserve.ProjectLab.ClientAPI.Config;
 using Softserve.ProjectLab.ClientAPI.Models;
 using System.Net;
@@ -67,6 +68,40 @@ namespace Softserve.ProjectLab.ClientAPI.Services
 
         }
 
+        public async Task<FileResult> GetWorkOrderReportCSV()
+        {
+            var workOrdersTask = GetWorkOrdersAsync();
+            var techniciansTask = _technicianService.GetTechniciansAsync();
+            var statusesTask = _statusService.GetStatusesAsync();
+            var workTypesTask = _workTypeService.GetWorkTypesAsync();
+
+            await Task.WhenAll(workOrdersTask, statusesTask, techniciansTask, workTypesTask);
+
+            var workOrders = workOrdersTask.Result;
+            var technicians = techniciansTask.Result;
+            var statuses = statusesTask.Result;
+            var workTypes = workTypesTask.Result;
+
+            //LINQ to join work orders with statuses, technicians, and work types
+
+            var query = from wo in workOrders
+                        join tech in technicians on wo.TechnicianId equals tech.TechnicianId
+                        join wt in workTypes on wo.WorkTypeId equals wt.Id
+                        join st in statuses on wo.StatusId equals st.Id
+                        where wo.EndTime.HasValue && wo.StartTime.HasValue
+                        select new ReportData
+                        {
+                            WorkOrderName = wo.WorkOrderName,
+                            TechnicianName = tech.Name,
+                            WorkType = wt.Name,
+                            Status = st.Name,
+                            EndTime = wo.EndTime.Value,
+                            StartTime =  wo.StartTime.Value,
+                            CreatedDate = wo.CreatedDate,
+                            Duration = wo.EndTime.Value - wo.StartTime.Value,
+                        };
+            return null;
+        }
 
     }
 }
