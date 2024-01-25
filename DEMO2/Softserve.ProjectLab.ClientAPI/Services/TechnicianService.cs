168371966f4763414a4e7cd489bd0da0a8f2f456
claudio <rainclaudio25@gmail.com>
2024-01-25 18:49:46 -0300

    Improving Technician Controller

--- DEMO2/Softserve.ProjectLab.ClientAPI/Services/TechnicianService.cs
+++ DEMO2/Softserve.ProjectLab.ClientAPI/Services/TechnicianService.cs
@@ -18,16 +18,29 @@ namespace Softserve.ProjectLab.ClientAPI.Services
         }
         public async Task<Technician[]> GetTechniciansAsync()
         {
-            return await _apiConnector.GetAsync<Technician[]>(ApiUrls.GetAllTechnicians);
+            try
+            {
+                return await _apiConnector.GetAsync<Technician[]>(ApiUrls.GetAllTechnicians);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error with GetTechnicians: {ex.Message}");
+                throw;
+            }
         }
         public async Task<Technician> GetTechnicianAsync(int technicianId)
         {
-
-            Technician technician = await _apiConnector.GetAsync<Technician>(ApiUrls.GetTechnicianById + technicianId);
-
-            return technician;
+            try
+            {
+                return await _apiConnector.GetAsync<Technician>(ApiUrls.GetTechnicianById + technicianId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error with GetTechnicianByID: {ex.Message}");
+                throw;
+            }
         }
-        public async Task<TechnicianDetails[]> GetTechnicianByNameAsync(string technicianName)
+        public async Task<List<TechnicianDetails> > GetTechnicianByNameAsync(string technicianName)
         {
             /*
             Implementation of technician search by name
@@ -65,74 +78,71 @@ namespace Softserve.ProjectLab.ClientAPI.Services
                     [  ]
             */
             //Technician[] technicians = await GetTechniciansAsync();
-            var workOrderService = _serviceProvider.GetService(typeof(IWorkOrderService)) as IWorkOrderService;
-            var statusService = _serviceProvider.GetService(typeof(IStatusService)) as IStatusService;
-            var workTypeService = _serviceProvider.GetService(typeof(IWorkTypeService)) as IWorkTypeService;
-           
-
-            var workOrdersTask = workOrderService.GetWorkOrdersAsync();
-            var techniciansTask = GetTechniciansAsync();
-            var statusesTask = statusService.GetStatusesAsync();
-            var workTypesTask = workTypeService.GetWorkTypesAsync();
-
-            await Task.WhenAll(workOrdersTask, statusesTask, techniciansTask, workTypesTask);
-
-            var workOrders = workOrdersTask.Result;
-            var technicians = techniciansTask.Result;
-            var statuses = statusesTask.Result;
-            var workTypes = workTypesTask.Result;
+            try
+            {
+                var workOrderService = _serviceProvider.GetService(typeof(IWorkOrderService)) as IWorkOrderService;
+                var statusService = _serviceProvider.GetService(typeof(IStatusService)) as IStatusService;
+                var workTypeService = _serviceProvider.GetService(typeof(IWorkTypeService)) as IWorkTypeService;
 
 
-            //   Test 1: Technicians with the same Name
-            /*
-                 Technician[] testTechnicians = new Technician[]
-                 {
-                     new Technician { Name = "Mauricio Sepulveda", TechnicianId = 1, Address = "Carlos Condell 5806, Valparaiso" },
-                     new Technician { Name = "Natalia Henriquez", TechnicianId = 2, Address = "Diego Portales 3666, Valparaiso" },
-                     new Technician { Name = "Natalia Henriquez", TechnicianId = 3, Address = "Arturo Prat 1861, Santiago" },
-                     new Technician { Name = "Ramon Sepulveda", TechnicianId = 4, Address = "Av. Matucana 9075, Santiago" },
-                     new Technician { Name = "Gabriel Rivas", TechnicianId = 5, Address = "Aníbal Pinto 578, Valparaiso" },
-                     new Technician { Name = "Diego Ardiles", TechnicianId = 6, Address = "Calle Blanco 7259, Santiago" },
-                 };
-            */
+                var workOrdersTask = workOrderService.GetWorkOrdersAsync();
+                var techniciansTask = GetTechniciansAsync();
+                var statusesTask = statusService.GetStatusesAsync();
+                var workTypesTask = workTypeService.GetWorkTypesAsync();
+
+                await Task.WhenAll(workOrdersTask, statusesTask, techniciansTask, workTypesTask);
+
+                var workOrders = workOrdersTask.Result;
+                var technicians = techniciansTask.Result;
+                var statuses = statusesTask.Result;
+                var workTypes = workTypesTask.Result;
 
 
+                //   Test 1: Technicians with the same Name
+                /*
+                     Technician[] testTechnicians = new Technician[]
+                     {
+                         new Technician { Name = "Mauricio Sepulveda", TechnicianId = 1, Address = "Carlos Condell 5806, Valparaiso" },
+                         new Technician { Name = "Natalia Henriquez", TechnicianId = 2, Address = "Diego Portales 3666, Valparaiso" },
+                         new Technician { Name = "Natalia Henriquez", TechnicianId = 3, Address = "Arturo Prat 1861, Santiago" },
+                         new Technician { Name = "Ramon Sepulveda", TechnicianId = 4, Address = "Av. Matucana 9075, Santiago" },
+                         new Technician { Name = "Gabriel Rivas", TechnicianId = 5, Address = "Aníbal Pinto 578, Valparaiso" },
+                         new Technician { Name = "Diego Ardiles", TechnicianId = 6, Address = "Calle Blanco 7259, Santiago" },
+                     };
+                */
 
-            // Get the list of all technicians and WorkOrders
+                // Get the list of all technicians and WorkOrders
 
+                // Filter technicians by name using LINQ
+                var filteredTechnicians = technicians
+                    .Where(t => t.Name.IndexOf(technicianName, StringComparison.OrdinalIgnoreCase) >= 0)
+                    .Select(tech => new TechnicianDetails
+                    {
+                        TechnicianId = tech.TechnicianId,
+                        Technician = tech.Name,
+                        Address = tech.Address,
 
+                        // Nest the corresponding WorkOrders
+                        WorkOrders = workOrders
+                                      .Where(wo => wo.TechnicianId == tech.TechnicianId)
+                                      .Select(w => new WorkOrderDetails
+                                      {
+                                          WorkOrderName = w.WorkOrderName,
+                                          Technician = tech.Name,
+                                          WorkType = workTypes.Where(wt => wt.Id == w.WorkTypeId).First().Name,
+                                          //     Status =        statuses.Where(s => s.Id == w.StatusId).First().Name,
+                                          EndTime = w.EndTime.HasValue ? (DateTimeOffset)w.EndTime.Value : (DateTimeOffset?)null,
+                                          StartTime = w.StartTime.HasValue ? (DateTimeOffset)w.StartTime.Value : (DateTimeOffset?)null
+                                      }).ToArray()
+                    }).ToList();
 
-            // Filter technicians by name using LINQ
-            var filteredTechnicians = technicians
-                //testTechnicians
-                .Where(t => t.Name.Equals(technicianName, StringComparison.OrdinalIgnoreCase))
-                .Select(tech => new TechnicianDetails
-                {
-                    TechnicianId = tech.TechnicianId,
-                    Technician = tech.Name,
-                    Address = tech.Address,
-
-                    // Nest the corresponding WorkOrders
-                    WorkOrders =  //workOrders
-                                  //.Where(wo => wo.Technician == tech.Name)                 
-                                  //.ToArray()
-                                   workOrders
-                                  .Where(wo => wo.TechnicianId == tech.TechnicianId)
-                                  .Select(w => new WorkOrderDetails
-                                  {
-                                      WorkOrderName = w.WorkOrderName,
-                                      Technician =    tech.Name,
-                                      WorkType =      workTypes.Where(wt => wt.Id == w.WorkTypeId).First().Name,
-                                 //     Status =        statuses.Where(s => s.Id == w.StatusId).First().Name,
-                                      EndTime =       w.EndTime.HasValue ? (DateTimeOffset) w.EndTime.Value : (DateTimeOffset?)null,
-                                      StartTime =     w.StartTime.HasValue ? (DateTimeOffset) w.StartTime.Value : (DateTimeOffset?)null
-                                  }).ToArray()
-
-
-                }).ToArray();
-
-            return filteredTechnicians;
-
+                return filteredTechnicians;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+                throw;
+            }
         }
     }
 
